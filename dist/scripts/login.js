b1876c79816681fe74cf0d3661d1bf6efcd2b3c4
CarlSaks@github.com <unconfigured@null.spigotmc.org>
2020-09-24 21:08:10 +0300

    changed login.html layout and it's css file to match requirements

---
+++ dist/scripts/login.js
@@ -0,0 +1,31 @@
+const loginButton = document.getElementById("login-button");
+var credentials = new Map();
+
+credentials.set("name@mail.com", "1234");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    var email = document.getElementById("email-input").value;
+    var passw = document.getElementById("password-input").value;
+
+    if (credentials.has(email) && credentials.get(email) === passw) {
+        
+        window.location.href = "../html/index.html";
+    }
+
+    if ( !(credentials.has(email))) {
+
+        document.getElementById("email-input").style.boxShadow = "0 0 3px red";
+        document.getElementById("password-input").style.boxShadow = "0 0 3px red";
+    }
+    else {
+
+        document.getElementById("email-input").style.boxShadow = "initial";
+    }
+
+    if ( !(credentials.get(email) === passw)) {
+
+        document.getElementById("password-input").style.boxShadow = "0 0 3px red";
+    }
+});
